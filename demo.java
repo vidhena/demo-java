57497c79133e57acd7f46737ea03f8083572c263
vidhena <vidhena16@gmail.com>
2026-05-27 18:15:59 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    
+    // The main method is the execution entry point for your application
+    public static void main(String[] args) {
+        
+        // Prints the string text directly to your console
+        System.out.println("Hello, World!");
+    }
+}
